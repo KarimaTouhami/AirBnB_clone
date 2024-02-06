b20c19182f309ab724155067f2def3cff2a88040
YsN23 <yy2369964@gmail.com>
2024-02-06 20:27:01 +0100

    First Part of Task1 is completed

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import uuid
 from datetime import datetime
 
@@ -9,14 +11,13 @@ class BaseModel():
 
     def __init__(self):
         """Constructor Method"""
-        self.id = str(uuid.uuid4()) #Define a UUID for each instance is created
-        self.created_at = datetime.now() #Get The Datetime of an instance when its created
-        self.updated_at = datetime.now() #it will be updated every time you change your object
+        # Define a UUID for each instance is created
+        self.id = str(uuid.uuid4())
+        # Get The Datetime of an instance when its created
+        self.created_at = datetime.now()
+        # it will be updated every time you change your object
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """String Representation"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
-
-
-
