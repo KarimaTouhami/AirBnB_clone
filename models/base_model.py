b176cef251ad807e3213758d3ba63edf43525e20
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-02-09 15:54:15 +0100

    Resolve conflicts

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 import uuid
 from datetime import datetime
+import json
 
 """BaseModel Class"""
 
@@ -9,15 +10,35 @@ from datetime import datetime
 class BaseModel():
     """The BaseModel Class, The Project Main Structure"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor Method"""
-        # Define a UUID for each instance is created
-        self.id = str(uuid.uuid4())
-        # Get The Datetime of an instance when its created
-        self.created_at = datetime.now()
-        # it will be updated every time you change your object
-        self.updated_at = datetime.now()
+
+        format_time = '%Y-%m-%dT%H:%M:%S.%f'
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, format_time)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String Representation"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the attribute"""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """Serialize The Object and Modifying attributes"""
+
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
