e6372d910a8ac3ba0dbb398c5919d323aedf478a
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-02-11 21:47:17 +0100

    fixed pycodestyle

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class StateTest(unittest.TestCase):
     """ class to test State class """
-    
+
     def test_state(self):
         """ test State class """
         new = State()
@@ -15,13 +15,13 @@ class StateTest(unittest.TestCase):
         self.assertIsInstance(new, "created_at")
         self.assertIsInstance(new, "updated_at")
         self.assertIsInstance(new, "name")
-        
+
         """ check if it have the correct class """
         self.assertIsInstance(new.id, str)
         self.assertIsInstance(new.created_at, datetime)
         self.assertIsInstance(new.updated_at, datetime)
         self.assertIsInstance(new.name, str)
-        
-        
+
+
 if __name__ == "__main__":
     unittest.main()
