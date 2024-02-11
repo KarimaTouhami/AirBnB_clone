4251c5e0109dc9bce170bf56c2207607d0be2f0e
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-02-11 21:43:40 +0100

    finished user and state classes

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,27 @@
+#!user/bin/python3
+""" test for state class """
+import unittest
+from models.state import State
+from datetime import datetime
+
+
+class StateTest(unittest.TestCase):
+    """ class to test State class """
+    
+    def test_state(self):
+        """ test State class """
+        new = State()
+        self.assertIsInstance(new, "id")
+        self.assertIsInstance(new, "created_at")
+        self.assertIsInstance(new, "updated_at")
+        self.assertIsInstance(new, "name")
+        
+        """ check if it have the correct class """
+        self.assertIsInstance(new.id, str)
+        self.assertIsInstance(new.created_at, datetime)
+        self.assertIsInstance(new.updated_at, datetime)
+        self.assertIsInstance(new.name, str)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
