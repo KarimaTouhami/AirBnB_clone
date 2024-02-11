d40923efa5388a848d87aff6f3c58ce5453c4acf
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-02-11 16:30:43 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,9 +4,10 @@ Unittest for the BaseModel class
 Test files by using the following command line:
 python3 -m unittest tests/test_models/test_base_model.py
 """
+from hashlib import new #to declare the new hash
 import unittest
 import datetime
-from base_model import BaseModel
+from models.base_model import BaseModel
 import datetime
 
 
