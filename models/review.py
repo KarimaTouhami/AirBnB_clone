f8d2910ebebc0fb3b9db8fde885df0fa0e40dd99
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-02-11 09:18:01 +0100

    More classes

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!user/bin/python3
+""" This module defines Review class """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Review Class """
+    place_id = ""
+    user_id = ""
+    text = ""
